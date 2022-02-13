69eee92e0e566491ed089904a4d7785fa3cb6620
Restye János Barnabás <barna0801@gmail.com>
2022-02-13 15:40:45 +0100

    Added tail
    plus fun

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 
+int tailX[100], tailY[100];
+int nTail;
+
 enum eDirection {
 	STOP = 0,
 	LEFT,
@@ -49,7 +52,16 @@ void Draw() {
 				cout << "F";
 			}
 			else {
-				cout << " ";
+				bool print = false;
+				for (int k = 0; k < nTail; k++) {
+					if (tailX[k] == j && tailY[k] == i) {
+						cout << "o";
+						print = true;
+					}
+				}
+				if (!print) {
+					cout << " ";
+				}
 			}
 
 			if (j == width - 1) {
@@ -89,6 +101,21 @@ void Input() {
 }
 
 void Logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
 	switch (dir) {
 	case LEFT:
 		x--;
@@ -106,19 +133,29 @@ void Logic() {
 		break;
 	}
 
-	if (x > width || x < 0 || y > height || y < 0) {
-		gameOver = true;
+	//if (x > width || x < 0 || y > height || y < 0) {
+	//	gameOver = true;
+	//}
+
+	if (x >= width) x = 1; else if (x < 0) x = width - 1;
+	if (y >= height) y = 1; else if (y < 0) y = height - 1;
+
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y) {
+			gameOver = true;
+		}
 	}
 
 	if (x == fruitX && y == fruitY) {
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+		nTail++;
 	}
 }
 
 int main() {
-	// srand(time(NULL));
+	srand(time(NULL));
 	Setup();
 	while (!gameOver) { 
 		Draw();
